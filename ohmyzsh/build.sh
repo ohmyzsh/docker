2e89b5e7296450f075b9829d74f53efc03735a3c
Marc Cornellà <hello@mcornella.com>
2021-06-11 15:34:39 +0200

    Rename ohmyzsh/ohmyzsh:master to ohmyzsh/ohmyzsh:main

--- ohmyzsh/build.sh
+++ ohmyzsh/build.sh
@@ -10,11 +10,16 @@ IMAGE="$(basename "$(pwd)")"
 
 # Get ohmyzsh stable releases
 versions="$(curl -sL https://api.github.com/repos/ohmyzsh/ohmyzsh/tags | sed -n 's/[^0-9.]*"name": "v\?\([^"]*\)"[^0-9.]*/\n\1/g;s/^\n//p')"
-versions="$versions master"
+versions="$versions main"
 
 # Build images
 for version in $versions; do
-    docker buildx build -t "$USERNAME/$IMAGE:$version" --build-arg OMZ_VERSION="$version" .
+    # Rename main to master for the branch name
+    # TODO: remove this logic once the master branch has been renamed
+    omz_version=$version
+    [[ $version = main ]] && omz_version=master
+
+    docker buildx build -t "$USERNAME/$IMAGE:$version" --build-arg OMZ_VERSION="$omz_version" .
 done
 
 # Tag latest image
